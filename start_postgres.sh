6ca8d13e33b9ed59d4d4257f08fdb74325a285db
James Gardner <99141052+JGardner01@users.noreply.github.com>
2025-05-20 23:22:29 +0100

    Altered script - added creation for sample database

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,3 +1,7 @@
 # PostgreSQL Startup
 echo "Starting PostgreSQL..."
 service postgresql start
+
+# Create DB user and database if not already existing
+su - postgres -c "psql -tc \"SELECT 1 FROM pg_roles WHERE rolname = 'jovyan'\" | grep -q 1 || psql -c \"CREATE USER jovyan WITH PASSWORD 'password';\""
+su - postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw jovyan || createdb jovyan -O jovyan"
